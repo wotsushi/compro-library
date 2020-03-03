5e8600542935586b5963562e58159d1236ab2922
wotsushi <wotsushi@gmail.com>
2020-03-03 22:08:26 +0900

    スニペットコードの変数を解析できるようにした
    ISSUE: #2

--- a.cpp
+++ a.cpp
@@ -2,6 +2,9 @@
 // begin
 int main()
 {
+    int var_x = 1;
+    int var_y = 2;
+    var_x++;
 }
 // end
 // agc
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 #[derive(serde::Serialize)]
 struct Snippet {
     name: String,
@@ -7,14 +9,31 @@ struct Snippet {
 }
 
 fn main() {
+    let src = std::fs::read_to_string("a.cpp").unwrap();
     let res = std::fs::write(
         "cpp.json",
         serde_json::to_string_pretty(&Snippet {
             name: "test".to_string(),
             prefix: "tst".to_string(),
             description: "first snippet".to_string(),
-            body: std::fs::read_to_string("a.cpp")
+            body: regex::Regex::new(r"var_(\w+)")
                 .unwrap()
+                .captures_iter(&src)
+                .map(|cap| (cap[0].to_string(), cap[1].to_string()))
+                .unique()
+                .enumerate()
+                .collect::<std::collections::HashMap<_, _>>()
+                .iter()
+                .fold(src, |s, (i, (var, name))| {
+                    s.replace(
+                        var,
+                        &format!(
+                            "${{{index}:{identifier}}}",
+                            index = i + 1,
+                            identifier = name
+                        ),
+                    )
+                })
                 .split("\n")
                 .skip_while(|&line| line != "// begin")
                 .skip(1)
