c332d816e81db5f3b88abebb105e04fd2813e87f
wotsushi <wotsushi@gmail.com>
2020-03-08 17:55:08 +0900

    lib: 浮動小数点数の桁数表示指定指定(C++)
    ISSUE: #7

---
+++ lib/snippet/precision/precision.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vi;
+#define REP(i, n) for (ll i = 0; i < (n); ++i)
+#define OUT(x) cout << (x) << endl;
+#define OUTA(a, n) \
+    REP(i, n) { cout << (a[i]) << (i == n - 1 ? "\n" : " "); }
+#define ALL(a) (a).begin(), (a).end()
+#define SORT(a) sort(ALL(a))
+#define RSORT(a) \
+    SORT(a);     \
+    reverse(ALL(a))
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+
+int main()
+{
+    ll var_15 = 15;
+    double var_x = 3.14159265358979323846264338327950288419716939937510582097494459;
+    // begin
+    cout << fixed << setprecision(var_15) << var_x << endl;
+    // end
+}
