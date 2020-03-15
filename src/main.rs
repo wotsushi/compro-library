cf01ad6fddff81cb8b3787ad02f12399e836aeea
wotsushi <wotsushi@gmail.com>
2020-03-15 22:19:58 +0900

    AOJからテストスイートをダウンロードするコマンドを追加
    input/outputのサイズが大きい場合、全データは取得されず代わりにterminatedという文字列が含まれる。
    そのようなテストケースは役に立たないのでスキップしている。
    
    ISSUE: #17

--- src/main.rs
+++ src/main.rs
@@ -13,18 +13,73 @@ struct Package {
     modules: Vec<String>,
 }
 
-fn main() {
-    let res = match clap::App::new("cmplib")
+#[derive(serde::Deserialize, Debug)]
+struct AOJHeader {
+    serial: i64,
+    name: String,
+    #[serde(rename(deserialize = "inputSize"))]
+    input_size: i64,
+    #[serde(rename(deserialize = "outputSize"))]
+    output_size: i64,
+    score: i64,
+}
+
+#[derive(serde::Deserialize, Debug)]
+struct AOJHeaderInfo {
+    #[serde(rename(deserialize = "problemId"))]
+    problem_id: String,
+    headers: Vec<AOJHeader>,
+}
+
+#[derive(serde::Deserialize, Debug)]
+struct AOJTestCase {
+    #[serde(rename(deserialize = "problemId"))]
+    problem_id: String,
+    serial: i64,
+    #[serde(rename(deserialize = "in"))]
+    input: String,
+    #[serde(rename(deserialize = "out"))]
+    output: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    match clap::App::new("cmplib")
         .about("compro-library CLI")
         .version("0.0.1")
         .subcommand(clap::App::new("snip").about("generate VSCode's snippets"))
+        .subcommand(
+            clap::App::new("fetch")
+                .about("fetch test cases from AOJ")
+                .arg(
+                    clap::Arg::with_name("id")
+                        .help("AOJ's problem ID")
+                        .takes_value(true)
+                        .required(true),
+                )
+                .arg(
+                    clap::Arg::with_name("package")
+                        .help("package to be added test suite")
+                        .takes_value(true)
+                        .required(true),
+                )
+                .arg(
+                    clap::Arg::with_name("module")
+                        .help("module to be added test suite")
+                        .takes_value(true)
+                        .required(true),
+                ),
+        )
         .get_matches()
-        .subcommand_name()
+        .subcommand()
     {
-        Some("snip") => std::fs::write(
-            "cpp.json",
-            serde_json::to_string_pretty(
-                &serde_yaml::from_str::<Vec<Package>>(&std::fs::read_to_string("lib.yml").unwrap())
+        ("snip", _) => {
+            std::fs::write(
+                "cpp.json",
+                serde_json::to_string_pretty(
+                    &serde_yaml::from_str::<Vec<Package>>(
+                        &std::fs::read_to_string("lib.yml").unwrap(),
+                    )
                     .unwrap()
                     .iter()
                     .map(|package| {
@@ -100,15 +155,41 @@ fn main() {
                     })
                     .flatten()
                     .collect::<std::collections::BTreeMap<_, _>>(),
-            )
-            .unwrap(),
-        ),
-        _ => {
-            println!("abc");
+                )
+                .unwrap(),
+            )?;
             Ok(())
         }
-    };
-    if res.is_err() {
-        panic!("error");
+        ("fetch", Some(fetch_matches)) => {
+            let id = fetch_matches.value_of("id").unwrap();
+            let header_info = reqwest::get(&format!(
+                "https://judgedat.u-aizu.ac.jp/testcases/{}/header",
+                id
+            ))
+            .await?
+            .json::<AOJHeaderInfo>()
+            .await?;
+            let package = fetch_matches.value_of("package").unwrap();
+            let module = fetch_matches.value_of("module").unwrap();
+            let dir = format!("lib/{}/{}/{}", package, module, id);
+            std::fs::create_dir_all(format!("{}/in", dir))?;
+            std::fs::create_dir_all(format!("{}/out", dir))?;
+            for header in header_info.headers {
+                let testcase = reqwest::get(&format!(
+                    "https://judgedat.u-aizu.ac.jp/testcases/{}/{}",
+                    id, header.serial
+                ))
+                .await?
+                .json::<AOJTestCase>()
+                .await?;
+                if !testcase.input.contains("terminated") && !testcase.output.contains("terminated")
+                {
+                    std::fs::write(format!("{}/in/{}", dir, header.serial), &testcase.input)?;
+                    std::fs::write(format!("{}/out/{}", dir, header.serial), &testcase.output)?;
+                }
+            }
+            Ok(())
+        }
+        _ => Ok(()),
     }
 }
