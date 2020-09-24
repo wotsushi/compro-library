13942b3b3fce64a66d4f9e0f415703d43cd943af
wotsushi <wotsushi@gmail.com>
2020-09-24 20:49:46 +0900

    fix test code

--- library/tests/rollinghash.test.cpp
+++ library/tests/rollinghash.test.cpp
@@ -1,14 +1,15 @@
 #define PROBLEM \
   "https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/all/ALDS1_14_B"
-#include "../library/rollinghash.hpp"
+#include "../rollinghash.hpp"
 
 int main() {
   STR(T, P);
   rollinghash rht(T);
   rollinghash rhp(P);
+  ll N = T.size();
   ll M = P.size();
   ii h = rhp.hash(0, M);
-  rep(i, T.size() - M + 1) {
+  rep(i, N - M + 1) {
     if (rht.hash(i, i + M) == h) {
       out(i);
     }
