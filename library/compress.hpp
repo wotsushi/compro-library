d4a9b995b2b9a4e5397493c8dcab8a247131d683
wotsushi <wotsushi@gmail.com>
2020-10-08 22:51:01 +0900

    Move: compress

--- library/compress.hpp
+++ library/compress.hpp
@@ -1,6 +1,14 @@
 #pragma once
 #include "template.hpp"
 
+/**
+ * 座標圧縮した整数列を返します。圧縮後の整数には0, 1, ...が割り当てられます。
+ * 圧縮前の整数列に負の値が含まれていても構いません。
+ * 計算量: O(|A|log|A|)
+ *
+ * @param A 座標圧縮対象の整数列
+ * @return 座標圧縮後の整数列
+ */
 vi compress(vi &A) {
   map<ll, ll> P;
   each(a, A) { P.insert({a, 0}); }
---
+++ library/tests/compress.cpp
@@ -0,0 +1,18 @@
+#include "../compress.hpp"
+
+int main() {
+  // 圧縮後の列は0が最小値となる
+  {
+    vi A{2, 7, 1, 8, 2};
+    vi actual = compress(A);
+    vi expected{1, 2, 0, 3, 1};
+    assert(actual == expected);
+  }
+  // 負の値が含まれていてもよい
+  {
+    vi A{-3, -1, -4, -1, 5};
+    vi actual = compress(A);
+    vi expected = vi{1, 2, 0, 2, 3};
+    assert(actual == expected);
+  }
+}
