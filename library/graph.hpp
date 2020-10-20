d40a83fd8f9cc56439f7fd718b7d64e165eccf72
wotsushi <wotsushi@gmail.com>
2020-10-20 22:30:30 +0900

    graphを移動

--- library/graph.hpp
+++ library/graph.hpp
@@ -1,6 +1,10 @@
 #pragma once
 #include "template.hpp"
 
+/**
+ * グラフを表すデータ構造です。
+ * 重み付き／なしの有向／無向グラフを表現できます
+ */
 template <typename W>
 struct graph {
   using E = pair<ll, W>;
@@ -10,13 +14,27 @@ struct graph {
   bool directed;
   vector<vector<E>> e;
 
-  graph() {}
-
+  /**
+   * 隣接リストを用いてグラフを作成します
+   * 計算量: O(n)
+   *
+   * @param n 頂点数
+   * @param m 辺数
+   * @param weighted 重み付きならばtrue
+   * @param directed 有向グラフならばtrue
+   */
   graph(ll n, ll m = 0, bool weighted = false, bool directed = false)
       : n(n), m(m), directed(directed), weighted(weighted) {
     e.assign(n + 1, vector<E>());
   }
 
+  /**
+   * グラフに辺を追加します
+   *
+   * @param i 辺の端点（有向グラフの場合は始点）
+   * @param j 辺の端点（有向グラフの場合は終点）
+   * @param w 辺の重み（重み付きでないグラフならば1）
+   */
   void add(ll i, ll j, W w = 1) {
     e[i].push_back(E(j, w));
     if (not directed) {
@@ -25,11 +43,7 @@ struct graph {
     ++m;
   }
 
-  vector<E> &operator[](int i)
-
-  {
-    return e[i];
-  }
+  vector<E> &operator[](int i) { return e[i]; }
 };
 
 template <typename W>
---
+++ library/tests/graph.test.cpp
@@ -0,0 +1,98 @@
+#define PROBLEM \
+  "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A"
+#include "../graph.hpp"
+
+int main() {
+  // 無向グラフ
+  {
+    graph<ll> G(4);
+    G.add(1, 2);
+    G.add(2, 3);
+    G.add(3, 4);
+    G.add(1, 4);
+    assert(G[1] == vector<ii>({ii(2, 1), ii(4, 1)}));
+    assert(G[2] == vector<ii>({ii(1, 1), ii(3, 1)}));
+    assert(G[3] == vector<ii>({ii(2, 1), ii(4, 1)}));
+    assert(G[4] == vector<ii>({ii(3, 1), ii(1, 1)}));
+  }
+  // 重み付き無向グラフ
+  {
+    graph<ll> G(4, 0, true);
+    G.add(1, 2, 9);
+    G.add(2, 3, 8);
+    G.add(3, 4, 7);
+    G.add(1, 4, 6);
+    assert(G[1] == vector<ii>({ii(2, 9), ii(4, 6)}));
+    assert(G[2] == vector<ii>({ii(1, 9), ii(3, 8)}));
+    assert(G[3] == vector<ii>({ii(2, 8), ii(4, 7)}));
+    assert(G[4] == vector<ii>({ii(3, 7), ii(1, 6)}));
+  }
+  // 有向グラフ
+  {
+    graph<ll> G(4, 0, false, true);
+    G.add(1, 2);
+    G.add(2, 3);
+    G.add(3, 4);
+    G.add(4, 1);
+    assert(G[1] == vector<ii>({ii(2, 1)}));
+    assert(G[2] == vector<ii>({ii(3, 1)}));
+    assert(G[3] == vector<ii>({ii(4, 1)}));
+    assert(G[4] == vector<ii>({ii(1, 1)}));
+  }
+  // 重み付き有向グラフ
+  {
+    graph<ll> G(4, 0, true, true);
+    G.add(1, 2, 9);
+    G.add(2, 3, 8);
+    G.add(3, 4, 7);
+    G.add(4, 1, 6);
+    assert(G[1] == vector<ii>({ii(2, 9)}));
+    assert(G[2] == vector<ii>({ii(3, 8)}));
+    assert(G[3] == vector<ii>({ii(4, 7)}));
+    assert(G[4] == vector<ii>({ii(1, 6)}));
+  }
+  // 標準入力から無向グラフを構築
+  {
+    graph<ll> G(4, 4);
+    string in = "1 2\n2 3\n3 4\n1 4\n";
+    istringstream is(in);
+    is >> G;
+    assert(G[1] == vector<ii>({ii(2, 1), ii(4, 1)}));
+    assert(G[2] == vector<ii>({ii(1, 1), ii(3, 1)}));
+    assert(G[3] == vector<ii>({ii(2, 1), ii(4, 1)}));
+    assert(G[4] == vector<ii>({ii(3, 1), ii(1, 1)}));
+  }
+  // 標準入力から重み付き無向グラフを構築
+  {
+    graph<ll> G(4, 4, true);
+    string in = "1 2 9\n2 3 8\n3 4 7\n1 4 6\n";
+    istringstream is(in);
+    is >> G;
+    assert(G[1] == vector<ii>({ii(2, 9), ii(4, 6)}));
+    assert(G[2] == vector<ii>({ii(1, 9), ii(3, 8)}));
+    assert(G[3] == vector<ii>({ii(2, 8), ii(4, 7)}));
+    assert(G[4] == vector<ii>({ii(3, 7), ii(1, 6)}));
+  }
+  // 標準入力から有向グラフを構築
+  {
+    graph<ll> G(4, 4, false, true);
+    string in = "1 2\n2 3\n3 4\n4 1\n";
+    istringstream is(in);
+    is >> G;
+    assert(G[1] == vector<ii>({ii(2, 1)}));
+    assert(G[2] == vector<ii>({ii(3, 1)}));
+    assert(G[3] == vector<ii>({ii(4, 1)}));
+    assert(G[4] == vector<ii>({ii(1, 1)}));
+  }
+  // 標準入力から重み付き有向グラフを構築
+  {
+    graph<ll> G(4, 4, true, true);
+    string in = "1 2 9\n2 3 8\n3 4 7\n4 1 6\n";
+    istringstream is(in);
+    is >> G;
+    assert(G[1] == vector<ii>({ii(2, 9)}));
+    assert(G[2] == vector<ii>({ii(3, 8)}));
+    assert(G[3] == vector<ii>({ii(4, 7)}));
+    assert(G[4] == vector<ii>({ii(1, 6)}));
+  }
+}
